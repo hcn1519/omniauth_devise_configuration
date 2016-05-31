718aab97f71c2d7591828d6dd6b6a3c2c89c0961
Hong, Chang Nam <hcn1519@gmail.com>
2016-05-31 04:36:13 +0000

    first commit

---
+++ app/controllers/user/omniauth_callbacks_controller.rb
@@ -0,0 +1,51 @@
+class User::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  
+  def self.provides_callback_for(provider)
+    class_eval %Q{
+      def #{provider}
+        @user = User.find_for_oauth(env["omniauth.auth"], current_user)
+
+        if @user.persisted?
+          sign_in_and_redirect @user, event: :authentication
+          set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
+        else
+          session["devise.#{provider}_data"] = env["omniauth.auth"]
+          redirect_to new_user_registration_url
+        end
+      end
+    }
+  end
+
+  [:instagram, :kakao, :naver, :facebook, :google_oauth2].each do |provider|
+    provides_callback_for provider
+  end
+
+  def after_sign_in_path_for(resource)
+      root_path
+  end
+  
+  
+  # You should also create an action method in this controller like this:
+  # def twitter
+  # end
+
+  # More info at:
+  # https://github.com/plataformatec/devise#omniauth
+
+  # GET|POST /resource/auth/twitter
+  # def passthru
+  #   super
+  # end
+
+  # GET|POST /users/auth/twitter/callback
+  # def failure
+  #   super
+  # end
+
+  # protected
+
+  # The path used when OmniAuth fails
+  # def after_omniauth_failure_path_for(scope)
+  #   super(scope)
+  # end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,53 @@
+class User < ActiveRecord::Base
+  
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+  
+  def self.find_for_oauth(auth, signed_in_resource = nil)
+
+    # user와 identity가 nil이 아니라면 받는다
+    identity = Identity.find_for_oauth(auth)
+    user = signed_in_resource ? signed_in_resource : identity.user
+    
+    # user가 nil이라면 새로 만든다.
+    if user.nil?
+      
+      # 이미 있는 이메일인지 확인한다.
+      email = auth.info.email
+      user = User.where(:email => email).first  
+      unless self.where(email: auth.info.email).exists?
+        
+        # 없다면 새로운 데이터를 생성한다.
+        if user.nil?
+          if auth.provider == "naver"
+            user = User.new(
+              name: auth.info.name,
+              email: auth.info.email,
+              password: Devise.friendly_token[0,20]
+            )
+          else
+            user = User.new(
+              name: auth.extra.raw_info.name,
+              email: auth.info.email,
+              password: Devise.friendly_token[0,20]
+            )
+          end  
+            
+            user.save!
+        end
+        
+      end
+    
+    end
+    
+    if identity.user != user
+      identity.user = user
+      identity.save!
+    end
+    user
+    
+  end
+  
+end
